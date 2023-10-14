6d0619c52fc2ffd553ce0e56701a805e7bbd7acf
talu <talita.santos96@hotmail.com>
2023-10-14 12:05:45 -0300

    [sync] Add herança de Filha na classe Mae

--- exercicios/para-sala/Exercício 3/Mae.js
+++ exercicios/para-sala/Exercício 3/Mae.js
@@ -3,17 +3,25 @@ class Mae {
     corDosOlhos;
     corDoCabelo;
 
-    constructor(nacionalidade, corDosOlhos, corDoCabelo){
+    constructor(nacionalidade, corDosOlhos, corDoCabelo) {
         this.nacionalidade = nacionalidade;
         this.corDosOlhos = corDosOlhos;
         this.corDoCabelo = corDoCabelo;
     }
 
-    mostrarCaracteristicas(){
-        console.log(`Esta mãe é ${this.nacionalidade}, tem olhos ${this.corDosOlhos} e cabelo ${this.corDoCabelo}.`)
+    mostrarCaracteristicas() {
+        console.log(`Nacionalidade: ${this.nacionalidade}, Cor dos olhos: ${this.corDosOlhos}, Cor do cabelo: ${this.corDoCabelo}.`)
     }
 }
 
 const minhaMae = new Mae("brasileira", "castanho escuro", "grisalho");
 console.log(minhaMae);
 minhaMae.mostrarCaracteristicas();
+
+
+class Filha extends Mae {}
+
+const eu = new Filha ("brasileira", "castanho escuro", "castanho claro");
+
+console.log(eu);
+eu.mostrarCaracteristicas();
